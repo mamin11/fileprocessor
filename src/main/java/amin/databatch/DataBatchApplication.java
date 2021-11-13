ae991ef2c0bc5c38935ffc4a3063bad2dcb7c7d4
mamin11 <abdimamin@gmail.com>
2021-11-13 22:24:33 +0000

    initial commit

---
+++ src/main/java/amin/databatch/DataBatchApplication.java
@@ -0,0 +1,13 @@
+package amin.databatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/amin/databatch/DataBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package amin.databatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
